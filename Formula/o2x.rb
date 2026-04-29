863999cc1924671e8cc448067f2764d5d5a5fcc1
Lucas Katayama <492687+lucaskatayama@users.noreply.github.com>
2026-04-29 13:49:39 -0300

    Update o2x formula to v0.2.1

--- Formula/o2x.rb
+++ Formula/o2x.rb
@@ -1,26 +1,26 @@
 class O2x < Formula
   desc "OAuth2 CLI for authentication with various OAuth2 flows"
   homepage "https://github.com/lucaskatayama/o2x"
-  version "0.2.0"
+  version "0.2.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/lucaskatayama/o2x/releases/download/v0.2.0/o2x-darwin-arm64.tar.gz"
-      sha256 "ffc1b048bec2403176986c24ae17d221ecc1329df96eb1181ec4ba0f31114731"
+      url "https://github.com/lucaskatayama/o2x/releases/download/v0.2.1/o2x-darwin-arm64.tar.gz"
+      sha256 "5112db40930588205d547488cebac6af1e8f875c547eabe8a5f29b48eab4eed8"
     else
-      url "https://github.com/lucaskatayama/o2x/releases/download/v0.2.0/o2x-darwin-amd64.tar.gz"
-      sha256 "03c4263664f49378ca330e753a5100d076d4ed8d273ff57fcf4a357dd6a520bb"
+      url "https://github.com/lucaskatayama/o2x/releases/download/v0.2.1/o2x-darwin-amd64.tar.gz"
+      sha256 "b26dfa76f8815130caf0f0fc161a7ac904486c1bed164918a36ce49ce8242cb8"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/lucaskatayama/o2x/releases/download/v0.2.0/o2x-linux-arm64.tar.gz"
-      sha256 "48557aa07a83c4f7ab511af795d97ce086bc55e2d9e9efba406ad99d11272355"
+      url "https://github.com/lucaskatayama/o2x/releases/download/v0.2.1/o2x-linux-arm64.tar.gz"
+      sha256 "91f3e6f4a62448948e02350ab89bb0ddbebab03d69451b3d426f1fe02cc010fe"
     else
-      url "https://github.com/lucaskatayama/o2x/releases/download/v0.2.0/o2x-linux-amd64.tar.gz"
-      sha256 "9e5e0ac30ac6010a72f3c91110dbdc9abbbaef669ede4b4de5fd352b2e67e3f6"
+      url "https://github.com/lucaskatayama/o2x/releases/download/v0.2.1/o2x-linux-amd64.tar.gz"
+      sha256 "7dda1970dae6f3063db28fc723ca1c008aaa1613bcece6b8fb24028e2247a88b"
     end
   end
 
